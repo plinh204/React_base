6ecb3ec7ad37f81a8785019bcfaa843386634a09
plinh204 <nguyenphuonglinh05052004@gmail.com>
2024-07-16 19:01:01 +0700

    first commit

---
+++ src/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return <div className='container'>
+    <Outlet/>
+  </div>
+};
+
+export default Layout
